fc7fd789652f707c3da1bda0fdde08c80e3ea084
Yuliia Soloviova <solov81yova@gmail.com>
2023-11-01 13:11:46 +0100

    make addition calculator

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Hello {
 
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    System.out.println(a + b);
   }
 
 }
