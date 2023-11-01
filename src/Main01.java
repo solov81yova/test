5e22df55b2e4a7a6d0a8bc393c8130ccac76aadf
Yuliia Soloviova <149204895+solov81yova@users.noreply.github.com>
2023-11-01 20:25:50 +0100

    Update Main01.java

--- src/Main01.java
+++ src/Main01.java
@@ -25,7 +25,7 @@ public class Main01 {
 
   public static void printArray(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
-        
+      System.out.print("Число из массива")  
       System.out.print(arr[i] + " ");
     }
   }
